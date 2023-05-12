09ec753593ff2fd98f8ff19e32dc8de1983284e5
sovilleda07 <sovilleda07@gmail.com>
2023-05-11 20:45:59 -0600

    Creando proyecto

---
+++ lib/hola_mundo_hn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hola_mundo_hn/version"
+
+module HolaMundoHn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_hola_mundo_hn.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestHolaMundoHn < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::HolaMundoHn::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
